44e7639da608a9b03907c17e0ea879f31a764acb
Joseph Mureithi <33313923+JoddieNgatz@users.noreply.github.com>
2021-07-27 01:32:14 +0300

    created server.js entry next models

---
+++ Users/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const cors = require('cors');
+
+const app = express();
+
+// var corsOptions = {
+//     origin: "http://localhost:8081"
+// };
+
+const db = require('./models/');
+db.mongoose.connect(db.url, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    console.log('connected to database');
+}).catch(err => {
+    console.log('problem connecting to db', err); process.exit;
+});
+
+app.use(cors(corsOptions));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.status(200).json({ mesaage: "API working Welcome." });
+
+})
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT} at http://localhost:${PORT}`);
+})
