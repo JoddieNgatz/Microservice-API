7371033ee5552001c3a340af61ea14802864db54
Joseph Mureithi <33313923+JoddieNgatz@users.noreply.github.com>
2021-07-28 19:51:17 +0300

    Refractor

---
+++ Users/controller/users.controller.js
@@ -0,0 +1,66 @@
+
+    console.log('in routes');
+
+
+    var model = require('../models/');
+    var controller = require('../controller/users.controller');
+    const usr = model.usr;
+    const bcrypt = require("bcrypt");
+
+
+exports.signUp = (req, res) => {
+        const body = req.body.email;
+        if (!body) {
+            res.status(404).json({ message: "Kindly fill in details username, email and password" });
+        }
+        else {
+            const nUser = new usr({
+                "username": req.body.username, //will use for userId
+                "email": req.body.email,
+                "password": bcrypt.hashSync(req.body.password, 3),
+            });
+            nUser.save((err) => {
+                if (err) {
+                    res.status(500).json({ err: err });
+                    return;
+                } else {
+                    res.status(200).json({ message: "User registered Successfully!" });
+                }
+            });
+        }
+    
+
+};
+
+exports.signIn = (req, res) => {
+       
+    usr.findOne({
+        email: req.body.email
+    }).exec((err, user) => {
+        if (err) {
+            res.status(500), json({ message: err });
+            return;
+        }
+        if (!user) {
+            res.status(404).json({ message: "User Not Found. Register" })
+        }
+
+        var passwordValid = bcrypt.compareSync(
+            req.body.password,
+            user.password
+        );
+        if (!passwordValid) {
+            res.status(401).json({
+                message: 'Invalid Passowrd'
+            });
+        }else{
+        
+        res.status(200).json({
+            message: "Logged In",
+            name: user.username,
+            email: user.email,
+        });
+        }
+    });
+
+};
--- Users/routes/user.router.js
+++ Users/routes/user.router.js
@@ -3,10 +3,10 @@ module.exports = app => {
 
     console.log('in routes');
 
-
-var model = require('../models/');
-const usr = model.usr;
-const bcrypt = require("bcryptjs");
+    var controller = require('../controller/users.controller');
+    // const usr = model.usr;
+    // const bcrypt = require("bcrypt");
+    
         
     
 /**
@@ -14,9 +14,7 @@ const bcrypt = require("bcryptjs");
  * @param - /register
  * @description - User can register passing json username,email,password
  */
-    app.post("/user/register", (req, res) => {
-       register(req, res);
-    });
+    app.post("/user/register", controller.signUp);
 
 
 /**
@@ -24,57 +22,8 @@ const bcrypt = require("bcryptjs");
  * @param - /signin
  * @description - User SignIn passing json email,password
  */
-    app.post("/user/signin", (req, res) => {
-        
-           
-       usr.findOne({
-            email: req.body.email
-        }).exec((err, user) => {
-            if (err) {
-                res.status(500), json({ message: err });
-                return;
-            }
-            if (!user) {
-                res.status(404).json({ message: "User Not Found. Register" })
-            }
-    
-            var passwordValid = bcrypt.compareSync(
-                req.body.password,
-                user.password
-            );
-            if (!passwordValid) {
-                res.status(401).json({
-                    message: 'Invalid Passowrd'
-                });
-            }
-            
-            res.status(200).json({
-                name: user.username,
-                email: user.email,
-            });
-        });
-    });
+    app.post("/user/signin", controller.signIn);
 
     
-    function register(req, res) {
-        const body = req.body.email;
-        if (!body) {
-            res.status(404).json({ message: "Kindly fill in details username, email and password" });
-        }
-        else {
-            const nUser = new usr({
-                "username": req.body.username, //will use for userId
-                "email": req.body.email,
-                "password": bcrypt.hashSync(req.body.password, 3),
-            });
-            nUser.save((err) => {
-                if (err) {
-                    res.status(500).json({ err: err });
-                    return;
-                } else {
-                    res.status(200).json({ message: "User registered Successfully!" });
-                }
-            });
-        }
-    }
+
 }
